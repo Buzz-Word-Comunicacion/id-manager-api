16584076699dcc31fc9fb6c0797e74ec450ca31c
Arturo Jimenez <arturo.jimenez@freejolitos.com>
2024-01-11 10:38:48 -0600

    no sandbox

--- helpers/web_scraper_cr.py
+++ helpers/web_scraper_cr.py
@@ -11,7 +11,7 @@ from .db import insert_scrap_data
 def obtenerDriver():
     #Definición del driver
     options = webdriver.ChromeOptions()
-    chrome_options.addArguments("--no-sandbox")
+    options.add_argument("--no-sandbox")
     options.add_argument('headless')
     driver = webdriver.Chrome(options=options)
     return driver
